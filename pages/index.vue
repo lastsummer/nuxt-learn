8751dda84681fadf52b908a752b25e0d36c82cd9
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-06-04 09:35:29 +0800

    add routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        my-first-nuxt-app
-      </h1>
-      <h2 class="subtitle">
-        My praiseworthy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <nuxt-link to="users/">user</nuxt-link>
     </div>
   </div>
 </template>
---
+++ pages/users/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Please enter an id and click the button.</h1>
+</template>
+
